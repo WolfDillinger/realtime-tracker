d96478e0262829ebfe9a97b8997da3e425cba012
Ahnaf Batayneh <ahnafbataineh97@gmail.com>
2025-10-12 03:23:59 +0300

    add rajhi

--- models/Rajhi.js
+++ models/Rajhi.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const rajhiSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   rajhiName: { type: String, required: true },
